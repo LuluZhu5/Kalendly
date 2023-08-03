74f887df374dc58f34abf41af014c27d6ec83b61
Ziming (Harry) Zhu <harry.zhu0325@gmail.com>
2023-08-03 00:22:11 -0700

    Add styles

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,14 @@
-import AuthForm from './components/Authform'
+import AuthForm from './components/Authform';
 
 export default function HomePage() {
     return (
-        <div>
-            <div>Sign in to your account</div>
+        <div className="flex flex-col justify-center min-h-full py-12 bg-gray-100 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h2 className="mt-6 text-3xl font-bold tracking-tight text-center text-gray-900">
+                    Sign in to your account
+                </h2>
+            </div>
             <AuthForm />
         </div>
-    )
+    );
 }
