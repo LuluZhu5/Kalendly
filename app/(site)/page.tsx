744e05690a40a5d94370c2c48d2d4557980195d9
Ziming (Harry) Zhu <harry.zhu0325@gmail.com>
2023-08-03 00:13:44 -0700

    Add Auth forms and fields

---
+++ app/(site)/page.tsx
@@ -0,0 +1,10 @@
+import AuthForm from './components/Authform'
+
+export default function HomePage() {
+    return (
+        <div>
+            <div>Sign in to your account</div>
+            <AuthForm />
+        </div>
+    )
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <div>Hello There</div>
-    </main>
-  )
-}
