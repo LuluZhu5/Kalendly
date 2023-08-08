655f8de01f40f51aa5d1edff7a86815bb322a297
Ziming (Harry) Zhu <harry.zhu0325@gmail.com>
2023-08-07 17:43:41 -0700

    Completed Auth and navbar

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,5 @@
-'use client';
-
-import { signOut } from 'next-auth/react';
-
 const Users = () => {
-    return <button onClick={() => signOut()}>Logout</button>;
+    return <div></div>;
 };
 
 export default Users;
