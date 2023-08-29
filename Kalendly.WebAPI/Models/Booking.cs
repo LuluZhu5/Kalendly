52b98c66304ac8e93bba70eb75911ef7e4c65a36
Lycoris <zac7e4@outlook.com>
2023-08-28 22:07:40 -0400

    KAL-8 Complete two models

--- Kalendly.WebAPI/Models/Booking.cs
+++ Kalendly.WebAPI/Models/Booking.cs
@@ -8,4 +8,29 @@ public class Booking : ICollections
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
+
+    [BsonElement("uid")]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Uid { get; set; } = null!;
+    
+    [BsonElement("title")]
+    public string Title { get; set; } = null!;
+
+    [BsonElement("description")]
+    public string? Description { get; set; }
+
+    [BsonElement("startTime")]
+    public DateTime StartTime { get; set; } = DateTime.MinValue;
+
+    [BsonElement("endTime")]
+    public DateTime EndTime { get; set; } = DateTime.MaxValue;
+
+    [BsonElement("location")]
+    public string? Location { get; set; }
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    [BsonElement("updatedAt")]
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
 }
--- Kalendly.WebAPI/Models/User.cs
+++ Kalendly.WebAPI/Models/User.cs
@@ -8,5 +8,37 @@ public class User : ICollections
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
+    
+    [BsonElement("name")]
+    public string? Name { get; set; }
 
+    [BsonElement("email")]
+    public string? Email { get; set; }
+
+    [BsonElement("emailVerified")]
+    public DateTime? EmailVerified { get; set; }
+
+    [BsonElement("image")]
+    public string? Image { get; set; }
+
+    [BsonElement("hashedPassword")]
+    public string? HashedPassword { get; set; }
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    [BsonElement("updatedAt")]
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    [BsonElement("bookings")]
+    public Booking[] Bookings { get; set; } = null!;
+
+    [BsonElement("openDays")]
+    public bool[] OpenDays { get; set; } = null!;
+
+    [BsonElement("openHours")]
+    public string[] OpenHours { get; set; } = null!;
+
+    [BsonElement("accounts")]
+    public Account[] Accounts { get; set; } = null!;
 }
