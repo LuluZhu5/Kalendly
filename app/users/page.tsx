7af2b186ace35ab44669013595065e05e9f781bb
Ziming (Harry) Zhu <harry.zhu0325@gmail.com>
2023-10-06 16:44:58 -0700

    update

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,22 +1,14 @@
 'use client';
 
-import Button from '@/app/components/Button';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 const Users: React.FC = () => {
     const { data: session } = useSession();
 
-    const handleAddBooking = () => {
-        console.log(session?.user?.id);
-    };
-
     return (
         <div>
             <div className="pt-[100px]">
-                <Button type="button" onClick={handleAddBooking}>
-                    Add a booking
-                </Button>
                 <Link href={`${session?.user?.id}/add-booking`}>
                     Add a booking
                 </Link>
@@ -24,5 +16,3 @@ const Users: React.FC = () => {
         </div>
     );
 };
-
-export default Users;
